61c5754e4460ff896d716e2f5d948585bdf7bb61
Cengaver <154662656+CodmetY@users.noreply.github.com>
2025-12-03 01:30:00 +0100

    define file type

---
+++ src/main/java/io/github/cengauver/cobol_plugin_intellij_idea/CobolFileType.java
@@ -0,0 +1,44 @@
+package io.github.cengauver.cobol_plugin_intellij_idea;
+
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public final class CobolFileType extends LanguageFileType {
+
+    public static final CobolFileType INSTANCE = new CobolFileType();
+
+    private CobolFileType() {
+        super(CobolLanguage.INSTANCE);
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "Cobol File";
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "Cobol language file";
+    }
+
+    @NotNull
+    @Override
+    public String getDefaultExtension() {
+        return "cbl";
+    }
+
+    // TODO: Add cob as an extension as well.
+
+    @Override
+    public Icon getIcon() {
+        return CobolIcons.FILE;
+    }
+
+}

4d84aa8aa7e74a61d16b5387576074bd0b8a74de
Cengaver <154662656+CodmetY@users.noreply.github.com>
2025-12-06 21:33:19 +0100

    implement Syntax Highlighter class and its factory classs

---
+++ src/main/java/io/github/cengauver/cobol_plugin_intellij_idea/CobolSyntaxHighlighter.java
@@ -0,0 +1,79 @@
+package io.github.cengauver.cobol_plugin_intellij_idea;
+
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.HighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IElementType;
+import io.github.cengauver.cobol_plugin_intellij_idea.psi.CobolTokenSets;
+import io.github.cengauver.cobol_plugin_intellij_idea.psi.CobolTypes;
+import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+
+public class CobolSyntaxHighlighter extends SyntaxHighlighterBase {
+
+    public static final TextAttributesKey KEYWORD = createTextAttributesKey("COBOL_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
+    public static final TextAttributesKey IDENTIFIER = createTextAttributesKey("COBOL_IDENTIFIER", DefaultLanguageHighlighterColors.IDENTIFIER);
+    public static final TextAttributesKey STRING = createTextAttributesKey("COBOL_STRING", DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey NUMBER =  createTextAttributesKey("COBOL_NUMBER", DefaultLanguageHighlighterColors.NUMBER);
+    public static final TextAttributesKey LINE_NUMBER = createTextAttributesKey("COBOL_LINE_NUMBER", DefaultLanguageHighlighterColors.LINE_COMMENT);
+    public static final TextAttributesKey RELATIONAL_OPERATOR = createTextAttributesKey("COBOL_RELATIONAL_OPERATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
+    public static final TextAttributesKey ARITHMETIC_OPERATOR = createTextAttributesKey("COBOL_ARITHMETIC_OPERATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
+    public static final TextAttributesKey DOT = createTextAttributesKey("COBOL_PUNCTUATION", DefaultLanguageHighlighterColors.DOT);
+
+    public static final TextAttributesKey COMMENT = createTextAttributesKey("COBOL_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+    public static final TextAttributesKey BAD_CHARACTER = createTextAttributesKey("COBOL_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+
+
+    private static final TextAttributesKey[] KEYWORD_KEYS = new TextAttributesKey[]{KEYWORD};
+    private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[]{STRING};
+    private static final TextAttributesKey[] NUMBER_KEYS = new TextAttributesKey[]{NUMBER};
+    private static final TextAttributesKey[] IDENTIFIER_KEYS = new TextAttributesKey[]{IDENTIFIER};
+    private static final TextAttributesKey[] LINE_NUMBER_KEYS = new TextAttributesKey[]{LINE_NUMBER};
+    private static final TextAttributesKey[] OPERATOR_KEYS = new TextAttributesKey[]{ARITHMETIC_OPERATOR, RELATIONAL_OPERATOR};
+    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+    private static final TextAttributesKey[] DOT_KEYS = new TextAttributesKey[]{DOT};
+
+    private static final TextAttributesKey[] BAD_CHARACTER_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
+    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+
+    @NotNull
+    @Override
+    public Lexer getHighlightingLexer() {
+        return new CobolLexerAdapter();
+    }
+
+    @Override
+    public TextAttributesKey @NotNull [] getTokenHighlights(IElementType tokenType) {
+        if (tokenType.equals(CobolTypes.STRING_LITERAL)) {
+            return STRING_KEYS;
+        }
+        if (tokenType.equals(CobolTypes.NUMBER_LITERAL)) {
+            return NUMBER_KEYS;
+        }
+        if (tokenType.equals(CobolTypes.IDENTIFIER)) {
+            return IDENTIFIER_KEYS;
+        }
+        if (tokenType.equals(CobolTypes.LINE_NUMBER)) {
+            return LINE_NUMBER_KEYS;
+        }
+        if (tokenType.equals(CobolTypes.PERIOD)) {
+            return DOT_KEYS;
+        }
+        if (CobolTokenSets.KEYWORDS.contains(tokenType)) {
+            return KEYWORD_KEYS;
+        }
+        if (CobolTokenSets.COMMENTS.contains(tokenType)) {
+            return COMMENT_KEYS;
+        }
+        if (tokenType.equals(TokenType.BAD_CHARACTER)) {
+            return BAD_CHARACTER_KEYS;
+        }
+        return EMPTY_KEYS;
+    }
+
+
+}
---
+++ src/main/java/io/github/cengauver/cobol_plugin_intellij_idea/CobolSyntaxHighlighterFactory.java
@@ -0,0 +1,18 @@
+package io.github.cengauver.cobol_plugin_intellij_idea;
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+public class CobolSyntaxHighlighterFactory extends SyntaxHighlighterFactory {
+
+    @NotNull
+    @Override
+    public SyntaxHighlighter getSyntaxHighlighter(Project project, VirtualFile virtualFile) {
+        return  new CobolSyntaxHighlighter();
+    }
+
+}
